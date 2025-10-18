0be3e12a60131ac51cd022e1e8183af40027dc30
Santosh Dhakal <santoshdhakal.inbox@gmail.com>
2025-10-18 16:57:07 +0545

    update to ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,7 +134,7 @@ data "aws_availability_zones" "available" {
 # EC2 Instance
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.web.id]
   subnet_id              = aws_subnet.public.id
